eef9cbdfa264d1866a28adc6e86d7570e4297ef2
Majkaz <majka.zem+git@gmail.com>
2018-10-15 23:29:00 +0200

    div/bashify.sh
    create script from history

---
+++ div/bashify.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+SCRIPT_NAME=$1
+NUMBER_OF_LINES_BACK=$2
+((NUMBER_OF_LINES_BACK++))
+PARENT_COMMAND="bin/"$3
+
+# echo shabang line and x number of lines of history to new script
+echo \#\!\/$PARENT_COMMAND > $SCRIPT_NAME.sh
+echo >> $SCRIPT_NAME.sh
+
+if [ "$PARENT_COMMAND" == "bin/zsh" ]
+then
+	# strip to command only
+	tail -n $NUMBER_OF_LINES_BACK ~/.zsh_history | 
+	tac | sed "1d" | tac |
+	sed 's/.\+;//' >> $SCRIPT_NAME.sh
+else
+	tail -n $NUMBER_OF_LINES_BACK ~/.bash_history |
+	tac | sed "1d" | tac >> $SCRIPT_NAME.sh
+fi
+
+chmod u+x $SCRIPT_NAME.sh
+
+# Open the newly created script with nano
+nano -l +3 $SCRIPT_NAME.sh
+
